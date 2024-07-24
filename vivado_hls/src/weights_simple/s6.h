d5035aefe749296fa9f7297b886afa407e5b89ea
mbileska <mb7126@princeton.edu>
2024-07-24 14:09:18 +0200

    test_larger_model

--- vivado_hls/src/weights_simple/s6.h
+++ vivado_hls/src/weights_simple/s6.h
@@ -1,6 +1,6 @@
 //Numpy array shape [4]
-//Min 1.276577711105
-//Max 1.459035158157
+//Min 0.015627283603
+//Max 0.044542305171
 //Number of zeros 0
 
 #ifndef S6_H_
@@ -9,7 +9,7 @@
 #ifndef __SYNTHESIS__
 norm_1_scale_t s6[4];
 #else
-norm_1_scale_t s6[4] = {1.2765777111, 1.3938595057, 1.4590351582, 1.3326176405};
+norm_1_scale_t s6[4] = {0.0361508243, 0.0378617980, 0.0445423052, 0.0156272836};
 #endif
 
 #endif
